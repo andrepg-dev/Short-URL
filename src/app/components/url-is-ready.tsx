4b4af6e92aa2a5bf9ae2e7d001d953493731de29
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-03-22 00:52:03 -0600

    Agregada la clase "md:select-text" al span del enlace para permitir la selección de texto en dispositivos de tamaño mediano.

--- src/app/components/url-is-ready.tsx
+++ src/app/components/url-is-ready.tsx
@@ -19,7 +19,7 @@ export default function URLIsReady() {
   return link !== "" && <div className="mt-5 border w-full bg-primary text-white py-5 animate-fade-in">
     <h4 className="text-sm md:text-base text-center font-light select-none text-pretty">Tu link está listo, toca el enlace para copiarlo</h4>
     <div className="text-center text-sm md:text-base">
-      <span className="text-center !text-orange-500 underline hover:bg-orange-900 hover:!text-white p-1 px-3 rounded transition cursor-pointer" onClick={handleOnClick}>{link}</span>
+      <span className="text-center !text-orange-500 underline hover:bg-orange-900 hover:!text-white p-1 px-3 rounded transition cursor-pointer select-none md:select-text" onClick={handleOnClick}>{link}</span>
     </div>
   </div>
 }
